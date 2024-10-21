1cd784ed60ca557a5c5b57069fd4697671371862
Manases Lovera <manaseslovera07@gmail.com>
2024-10-21 18:40:16 -0400

    Disable Jekyll for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'static',
+    build: {
+      outDir: 'docs', // Change the output directory to 'docs'
+    },
+    site: 'https://manases.dev', // Replace with your custom domain
+  });
